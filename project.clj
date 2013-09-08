29a7c7416e616021a82335debb1549fdaa09b68f
Simone Mosciatti <simone@mweb.biz>
2013-09-08 20:43:09 +0200

    add uri

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject clj-facebook-graph "0.4.0"
+(defproject clj-facebook-graph "0.4.0.siscia-local"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [ring "1.2.0"]
                  [clj-http "0.3.6"]
                  [clj-oauth2 "0.2.0"]
-                 [compojure "1.1.5"]]
-  :profile {:dev {
-                  :dependencies  [[ring/ring-devel "1.0.1"]
+                 [compojure "1.1.5"]
+                 [uri "1.1.0"]]
+  :profile {:dev {:dependencies  [[ring/ring-devel "1.0.1"]
                                   [ring/ring-jetty-adapter "1.0.1"]
                                   [compojure "1.0.1"]]}}
 
