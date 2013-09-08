41436dc527f7f41fe7857819586ea842849fe14f
Simone Mosciatti <simone@mweb.biz>
2013-09-08 19:21:09 +0200

    update version, add some dependecies necessary for test.example, bring it to lein2

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject clj-facebook-graph "0.4.0"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.1.2"  :exclude [org.clojure/clojure]]
-                 [ring/ring-core "1.0.1"  :exclude [org.clojure/clojure]]
-
+                 [org.clojure/data.json "0.1.2"]
+                 [ring "1.2.0"]
                  [clj-http "0.3.6"]
-                 [clj-oauth2 "0.2.0"]]
-
-  :dev-dependencies [[ring/ring-devel "1.0.1"  :exclude [org.clojure/clojure]]
-                     [ring/ring-jetty-adapter "1.0.1"  :exclude [org.clojure/clojure]]
-                     [compojure "1.0.1"  :exclude [org.clojure/clojure]]]
+                 [clj-oauth2 "0.2.0"]
+                 [compojure "1.1.5"]]
+  :profile {:dev {
+                  :dependencies  [[ring/ring-devel "1.0.1"]
+                                  [ring/ring-jetty-adapter "1.0.1"]
+                                  [compojure "1.0.1"]]}}
 
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"}
 
